ecaf37ebd15e4727c81d520775943c9e7ebcdc10
awm.builds <81087554+awm-builds@users.noreply.github.com>
2025-06-25 14:52:00 -0400

    feat: Added User model in Core layer with GUID primary keys

---
+++ StratoFlow.Core/Models/User.cs
@@ -0,0 +1,10 @@
+// StratoFlow.Core/Models/User.cs
+namespace StratoFlow.Core.Models
+{
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string Username { get; set; }
+        public string Role { get; set; }
+    }
+}
